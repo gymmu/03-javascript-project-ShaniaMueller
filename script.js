91eb0f25dc4a61843f6ebaf53cb208deb01a1ca3
shania.müller <e236316@edu.sbl.ch>
2023-11-06 11:07:22 +0100

    aufgabe 3

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ result.push(currentElement)
 return result.join("")
 }
 
+
+//aufgabe 2: Nur Grossbuchstaben, Grossschreibung
 export function aufgabe02(args) {
 const input = args 
 const result = []
@@ -29,5 +31,33 @@ result.push(capitalizedElement)
 }
 return result.join("")
 
-
 }
+
+
+//aufgabe 3: neuer Button -> 
+export function aufgabe03(args) {
+const input = args 
+const result = []
+let count = 0
+ 
+//soll nichts ausser ein kleines 'e' enthalten
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+if (currentElement === 'e'){
+  count = count + 1
+}
+ 
+// soll die grossen 'E' zählen
+else if (currentElement === 'E'){
+  count = count + 1
+}
+  }
+  return count
+}
+ 
+ 
+ 
+
+
+
+
