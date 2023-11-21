25d4062170146aa4b012a1727436f1d0b904c5ca
shania.müller <e236316@edu.sbl.ch>
2023-11-21 08:56:44 +0100

    versuch aufgabe 6

--- script.js
+++ script.js
@@ -237,3 +237,31 @@ export function aufgabe13 (args) {
        }}
     return true
   }
+
+
+  //aufgabe 6:
+  export function aufgabe06(args) {
+    const input = args
+      const result = []
+    let capitalLetters = 0
+    
+    
+    for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    
+    //prüft ob es einen Grossbuchstaben gibt
+    if (currentElement=== ".") {}
+    else if (currentElement===" "){}
+    
+    //wenn ein Element gleich ist, dann rechnet es plus 1.
+    else if (currentElement === currentElement.toUpperCase()) 
+    {capitalLetters++}
+    }
+    
+    if (capitalLetters > 0) {
+      return true
+    } else {
+       return false
+    }
+    
+    }
