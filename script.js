2dba025e3da958ccd60d26273ecb193fe185cf7f
shania.müller <e236316@edu.sbl.ch>
2023-11-21 08:12:41 +0100

    aufgabe 10

--- script.js
+++ script.js
@@ -216,3 +216,24 @@ export function aufgabe13 (args) {
 
     return result.join("")
   }
+
+//aufgabe 10:
+  export function aufgabe10(args) {
+    const input = args
+   
+    if (input.length !== 7) return false
+    if (input[0] !== "#") return false
+ 
+    for (let i = 1; i < input.length; i++) {
+      const currentElement = input[i]
+      const ascii = currentElement.charCodeAt(0)
+
+
+      if (48 <= ascii && ascii <= 57) {
+       } else if (65 <= ascii && ascii <= 70) {
+       }   
+       else {
+            return false
+       }}
+    return true
+  }
